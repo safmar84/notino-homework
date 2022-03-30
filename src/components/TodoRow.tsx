7bddb50a0c68e8ea6138bc634dad5e6ec7101bb4
Martin Šafář <schafi@Martins-MacBook-Pro-2.local>
2022-03-30 20:41:32 +0200

    Rewrite TodoRow component as functional. Used memo instead shouldComponentUpdate.

--- src/components/TodoRow.tsx
+++ src/components/TodoRow.tsx
@@ -1,27 +1,18 @@
-import React from 'react'
+import { memo } from 'react'
 
-class TodoRow extends React.Component<any> {
-	shouldComponentUpdate(prevProps: any) {
-	if(this.props !== prevProps) {
-		return true
-	}
-		return false
-	}
+const TodoRow = (props: any) => {
 
-	handleOnClick() {
+	const handleOnClick = () => {
 		window.location.href = '/detail'
 	}
 
-	render() {
-
 	return (
 		<div>
-			<div onClick={this.handleOnClick}>
-			{this.props.todo.title}
+			<div onClick={handleOnClick}>
+				{props.todo.title}
 			</div>
 		</div>
-	);
-	}
+	)
 }
 
-export default TodoRow
+export default memo(TodoRow)
